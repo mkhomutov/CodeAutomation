1d80ed8367c015c38101a224fd4b45260044d132
Max <mkhomutov@gmail.com>
2021-09-01 15:23:36 +0300

    Add initial code

---
+++ src/CodeAutomationConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeAutomationConsole
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
