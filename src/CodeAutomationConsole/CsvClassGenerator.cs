e4146d58e14007d6be223372a0e10d9dac29a77f
Kirill Potemkin <nullemotion@gmail.com>
2021-09-14 15:22:21 +0300

    Classes generation by application

---
+++ src/CodeAutomationConsole/CsvClassGenerator.cs
@@ -0,0 +1,69 @@
+﻿namespace CodeAutomationConsole
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.IO;
+
+    public class CsvClassGenerator
+    {
+        private readonly String _nameSpace;
+        private readonly String _className;
+        private readonly String _fileName;
+        private readonly String[] _fields;
+        private readonly String[] _properties;
+
+        public CsvClassGenerator(string nameSpace, string path)
+        {
+            _nameSpace = nameSpace;
+            _className = path.Split('\\').Last().Split('.').First();
+            _fileName = _className + ".cs";
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line = sr.ReadLine();
+                _properties = line.Split(',').Select(x => FirstLetterDown(x)).ToArray();
+                _fields = _properties.Select(x => "_" + x).ToArray();
+            }
+        }
+
+        public string GenerateClassCode()
+        {
+            string ConstructorTitle = _className + "(" + _properties.Select(x => "string " + x).ToArray().Aggregate((x, y) => x + ", " + y) + ")";
+            string ConstructorAssignments = _properties.Select(x => $"_{x} = {x};").Aggregate((x, y) => JoinLines(x, y, 3));
+            string Fields = _properties.Select(x => $"private readonly string _{x};").Aggregate((x, y) => JoinLines(x, y, 2));
+            string Properties = _properties.
+                Select(x => $"public string {FirstLetterUp(x)}\n{{\n\tget => _{x};\n}}".Split('\n').Aggregate((x, y) => JoinLines(x, y, 2))).
+                Aggregate((x, y) => JoinLines(x, y, 2));
+
+            string Code =  @$"namespace {_nameSpace}
+{{
+    using System;
+
+    public class {_className}
+    {{
+        // Fields
+        {Fields}
+
+        // Constructor
+        public {ConstructorTitle}
+        {{
+            {ConstructorAssignments}
+        }}
+
+        // Properties
+        {Properties}
+    }}
+}}
+";
+            return Code;
+        }
+
+        private string FirstLetterDown(string field) => field[0].ToString().ToLower() + String.Join("", field.Skip(1));
+
+        private string FirstLetterUp(string field) => field[0].ToString().ToUpper() + String.Join("", field.Skip(1));
+
+        private string JoinLines(string line1, string line2, int tabs) => line1 + "\r\n" + string.Concat(Enumerable.Repeat("\t", tabs)) + line2;
+
+    }
+}
--- src/CodeAutomationConsole/Program.cs
+++ src/CodeAutomationConsole/Program.cs
@@ -1,12 +1,43 @@
 ﻿namespace CodeAutomationConsole
 {
     using System;
+    using System.IO;
+    using System.Linq;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This application will generate C# classes based on csv files");
+            Console.WriteLine("Please enter path to your csv files:");
+
+            string Path = Console.ReadLine();
+            Path = Path.EndsWith('\\') ? Path : Path + "\\";
+            string[] Files = Directory.GetFiles(Path, "*.csv");
+
+            if (Files.Length == 0)
+            {
+                Console.WriteLine("No one csv file founded in this catalog");
+                System.Environment.Exit(1);
+            }
+
+            string ProjectName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
+
+            Console.WriteLine("\nGeneration started\n");
+
+            foreach (string file in Files)
+            {                
+                string NewFileName = file.Split('\\').Last().Split('.').First() + ".cs";
+                CsvClassGenerator Csv = new CsvClassGenerator(ProjectName, file);
+                string GeneratedClass = Csv.GenerateClassCode();
+                
+                using (FileStream fstream = new FileStream($"{Path}{NewFileName}", FileMode.Create))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes(GeneratedClass);
+                    fstream.Write(array, 0, array.Length);
+                    Console.WriteLine($"Generated class: {Path}{NewFileName}"); 
+                }
+            }    
         }
     }
 }
